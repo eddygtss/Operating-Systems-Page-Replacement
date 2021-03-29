3043f3f303f14be76912c65e1cb6155b13ed62ba
Eddy <eddyh95@gmail.com>
2021-03-29 16:35:32 -0500

    Implemented a checker for FIFO page faults.

--- page_replacement.py
+++ page_replacement.py
@@ -6,9 +6,34 @@ parser.add_argument('--num_frames', type=int, help='integer that defines size of
 args = parser.parse_args()
 
 
-def check():
+def check(requests, frames):
+    print(requests)
+
+    i = 0
+    j = 0
+    loop = 0
+    frame = []
+
+    for char in requests:
+        if len(frame) < frames:
+            frame.append(char)
+            j += 1
+
+        if char != frame[i]:
+            frame.pop(i)
+            frame.insert(i, char)
+            j += 1
+        print(frame)
+
+        if i == 2:
+            i = 0
+        else:
+            i += 1
+        loop += 1
+    print(j)
+    print(loop)
 
 
 if __name__ == '__main__':
-    requested = []
-
+    requested = [char for char in args.ref_string]
+    check(requested, args.num_frames)
