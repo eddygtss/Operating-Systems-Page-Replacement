5a6ec9a136ebe16e43e39fbcf106c9cd54ffaff2
Eddy <eddyh95@gmail.com>
2021-03-29 15:43:07 -0500

    Initial commit for homework 10.

---
+++ page_replacement.py
@@ -0,0 +1,14 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ref_string', type=str, help='string of integers')
+parser.add_argument('--num_frames', type=int, help='integer that defines size of memory to allocate.')
+args = parser.parse_args()
+
+
+def check():
+
+
+if __name__ == '__main__':
+    requested = []
+
