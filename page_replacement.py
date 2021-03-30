42d27684dfb730d4ce477754db7b38fd9c36d053
Eddy <eddyh95@gmail.com>
2021-03-30 17:35:10 -0500

    Added comments to code.

--- page_replacement.py
+++ page_replacement.py
@@ -1,5 +1,4 @@
 import argparse
-import queue
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ref_string', type=str, help='string of integers')
@@ -8,25 +7,30 @@ args = parser.parse_args()
 
 
 def check(requests, page_frames):
-    i = 0
-    j = 0
+    index = 0
+    faults = 0
     frame = []
 
+    # We will check every item in requests list
     for page in requests:
-        if page not in frame and len(frame) != 3:
-            frame.insert(i, page)
-            i += 1
-            j += 1
+        # This first conditional is to fill the frame list with the first 3 items, we in
+        if page not in frame and len(frame) != page_frames:
+            frame.insert(index, page)
+            index += 1
+            faults += 1
+        # Second conditional will remove the item at the index to be removed according to (FIFO), inserts the new
+        # item at the new location
         elif page not in frame:
-            frame.pop(i)
-            frame.insert(i, page)
-            j += 1
-            i += 1
+            frame.pop(index)
+            frame.insert(index, page)
+            index += 1
+            faults += 1
 
-        if i == page_frames:
-            i = 0
+        # So we can constraint the list to the size of the number of frames requested we set the index back to 0
+        if index == page_frames:
+            index = 0
 
-    print(str(j) + ' faults generated')
+    print(str(faults) + ' faults generated')
 
 
 if __name__ == '__main__':
