f7574696b61f3521fff0e5d601c7622d9a496b37
Eddy <eddyh95@gmail.com>
2021-03-30 17:15:25 -0500

    Reworked algorithm for FIFO

--- page_replacement.py
+++ page_replacement.py
@@ -1,45 +1,32 @@
 import argparse
+import queue
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ref_string', type=str, help='string of integers')
-parser.add_argument('--num_frames', type=int, help='integer that defines size of memory to allocate.')
+parser.add_argument('--num_frames', type=int, help='integer that defines size of frame.')
 args = parser.parse_args()
 
 
-def check(requests, frames):
-    print(requests)
-
+def check(requests, page_frames):
     i = 0
     j = 0
-    loop = 0
     frame = []
 
-    for char in requests:
-
-        temp = j
-        if len(frame) < frames:
-            frame.append(char)
-            j += 1
-        elif char != frame[i]:
-            frame.pop(i)
-            frame.insert(i, char)
+    for page in requests:
+        if page not in frame and len(frame) != 3:
+            frame.insert(i, page)
             i += 1
             j += 1
-        if i > 2:
+        elif page not in frame:
+            frame.pop(i)
+            frame.insert(i, page)
+            j += 1
+            i += 1
+
+        if i == page_frames:
             i = 0
-        print(str(frame) + ' index: ' + str(i) + ' faults: ' + str(j))
 
-
-
-        #elif j == temp:
-            #continue
-        #else:
-            #i += 1
-
-        #print({temp}, {j})
-        loop += 1
-
-    print(loop)
+    print(str(j) + ' faults generated')
 
 
 if __name__ == '__main__':
